b680efe8377d3e656a3ec16278ac25fecd833fa3
0x5 <oreoluwaakinbo.oa@gmail.com>
2026-07-03 15:19:25 +0100

    enable zsh

--- configuration.nix
+++ configuration.nix
@@ -215,7 +215,8 @@
     nativeMessagingHosts.packages = [ pkgs.firefoxpwa ];
   };
 
-  programs.noctalia-shell.enable = true;
+  programs.zsh.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
