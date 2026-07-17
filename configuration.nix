371cf0fe92f2503e8e53ed3449113754fac8490c
0x5 <oreoluwaakinbo.oa@gmail.com>
2026-07-17 08:56:04 +0100

    enable appimage

--- configuration.nix
+++ configuration.nix
@@ -248,5 +248,6 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
+  programs.appimage.enable = true;
 
 }
