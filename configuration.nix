4ec298cd89f13598025aa38ba6c6616fac3ccf71
0x5 <oreoluwaakinbo.oa@gmail.com>
2026-07-21 14:32:22 +0100

    enable mime for xdg

--- configuration.nix
+++ configuration.nix
@@ -241,6 +241,10 @@
     };
   };
 
+  xdg.mime {
+  	enable = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
